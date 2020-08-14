cf9988e4957e2e66680e4f9832164059f52c903e
Diana Pulatova <dianapulatovaa@gmail.com>
2020-08-14 18:19:44 -0400

    added icons

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+//   });
+
+// parallax for pictures
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
+
+// for drop down menu
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+  // for the form
+
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
+      
+        
